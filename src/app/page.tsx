89a759f96b13f937b4f00fc830876367e4b2b1ac
jlauza <lauzajayson@gmail.com>
2025-02-14 16:41:33 +0800

    next, mantine and basic home page

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import Link from "next/link";
+import { Button } from "@mantine/core";
+import { HeaderMegaMenu } from "./site-components/header";
+
+export default function Home() {
+  return <HeaderMegaMenu />;
+}
